7e47a27a16f6b758d8013c377935743c368c4a2f
ksn38 <ksn38@yandex.ru>
2022-12-22 19:22:27 +0300

    -

---
+++ MallardDuck.java
@@ -0,0 +1,13 @@
+//package ducks;
+
+public class MallardDuck extends Duck {
+  
+  public MallardDuck() {
+    quackBehavior = new Quack();
+    flyBehavior = new FlyWithWings();
+  }
+  
+  public void display() {
+    System.out.println("Im real MallardDuck");
+  }
+}
