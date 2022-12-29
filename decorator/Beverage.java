1fc410bf91638426e370c7f278e94e3151833991
ksn38 <ksn38@yandex.ru>
2022-12-29 20:33:38 +0300

    -

---
+++ decorator/Beverage.java
@@ -0,0 +1,9 @@
+public abstract class Beverage {
+  String description = "unknown beverage";
+  
+  public String getDescription() {
+    return description;
+  }
+  
+  public abstract double cost();
+}
---
+++ decorator/StarbuzzCoffee.java
@@ -0,0 +1,11 @@
+public class StarbuzzCoffee{
+  public static void main(String args[]){
+    Beverage beverage = new Espresso();
+    System.out.println(beverage.getDescription() + " $" + beverage.cost());
+    
+    Beverage beverage2 = new HouseBlend();
+    System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+    beverage2 = new Mocha(beverage2);
+    System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+  }
+}
